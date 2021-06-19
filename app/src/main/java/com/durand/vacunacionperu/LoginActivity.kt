43cee65ef519d9bc66f2fa29c95fe27f43c1b0de
JosueDurand96 <josue.durand8@gmail.com>
2021-06-19 17:37:03 -0500

    avance

--- app/src/main/java/com/durand/vacunacionperu/LoginActivity.kt
+++ app/src/main/java/com/durand/vacunacionperu/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.durand.vacunacionperu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginButton.setOnClickListener {
+            val intent = Intent(applicationContext, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/durand/vacunacionperu/MainActivity.kt
+++ app/src/main/java/com/durand/vacunacionperu/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.durand.vacunacionperu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TIMER
+        val duration_splash = 3000
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, duration_splash.toLong())
     }
 }
