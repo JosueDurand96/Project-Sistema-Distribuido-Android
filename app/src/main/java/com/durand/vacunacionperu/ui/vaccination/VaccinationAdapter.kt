a2c78d252f5d7bc098a90906fa374ad1c1db1846
JosueDurand96 <josue.durand8@gmail.com>
2021-06-20 23:17:18 -0500

    listar vaccination

--- app/src/main/java/com/durand/vacunacionperu/ui/vaccination/VaccinationAdapter.kt
+++ app/src/main/java/com/durand/vacunacionperu/ui/vaccination/VaccinationAdapter.kt
@@ -1,4 +1,63 @@
 package com.durand.vacunacionperu.ui.vaccination
 
-class VaccinationAdapter {
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.durand.domain.model.vaccination.VaccinationResponseModel
+import com.durand.vacunacionperu.R
+
+
+class VaccinationAdapter(
+    private val activity: Activity,
+    private val listLine: List<VaccinationResponseModel>
+) :
+    RecyclerView.Adapter<VaccinationAdapter.LineViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LineViewHolder {
+        return LineViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_vaccination,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return listLine.size
+    }
+
+    private var mOnClickSelectedPedidosPendientes: OnClickSelectedPedidosPendientes? = null
+
+    override fun onBindViewHolder(holder: LineViewHolder, position: Int) {
+
+        holder.fabricTextView.text = listLine[position].s_fabricante
+        holder.nameTextView.text = listLine[position].s_nombre
+//        holder.buttonConstraintLayout.setOnClickListener {
+//            mOnClickSelectedPedidosPendientes?.onSelectPedidosPendientes(
+//                    listLine[position].id,
+//                    listLine[position].title!!
+//            )
+//        }
+
+    }
+
+    class LineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+        var fabricTextView: TextView = itemView.findViewById(R.id.fabricTextView)
+
+    }
+
+    fun setListenerItemSelected(setOnClickSelectedPedidosPendientes: OnClickSelectedPedidosPendientes) {
+        mOnClickSelectedPedidosPendientes = setOnClickSelectedPedidosPendientes
+    }
+
+    interface OnClickSelectedPedidosPendientes {
+        fun onSelectPedidosPendientes(id: Int?, name: String)
+    }
+
 }
--- app/src/main/java/com/durand/vacunacionperu/ui/vaccination/VaccinationFragment.kt
+++ app/src/main/java/com/durand/vacunacionperu/ui/vaccination/VaccinationFragment.kt
@@ -1,5 +1,6 @@
 package com.durand.vacunacionperu.ui.vaccination
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,14 +10,20 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.durand.domain.model.vaccination.VaccinationResponseModel
 import com.durand.helper.base.BaseFragment
 import com.durand.vacunacionperu.R
 import com.durand.vacunacionperu.util.ScreenState
+import kotlinx.android.synthetic.main.fragment_vaccination.*
 
 class VaccinationFragment : BaseFragment() {
 
     private lateinit var vaccinationViewModel: VaccinationViewModel
-    private lateinit var vaccinationAdapter :VaccinationAdapter
+    private lateinit var vaccinationAdapter: VaccinationAdapter
+    private lateinit var vaccinationRecyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,6 +32,7 @@ class VaccinationFragment : BaseFragment() {
         vaccinationViewModel =
             ViewModelProvider(this).get(VaccinationViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_vaccination, container, false)
+        vaccinationRecyclerView = root.findViewById(R.id.vaccinationRecyclerView)
 //        val textView: TextView = root.findViewById(R.id.text_slideshow)
 //        vaccinationViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
@@ -34,9 +42,14 @@ class VaccinationFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vaccinationViewModel.getVaccination()
+        initGetVaccination()
         vaccinationViewModel.state.observe(::getLifecycle, ::getVaccination)
     }
+    private fun initGetVaccination(){
+        vaccinationProgressBar.visibility = View.VISIBLE
+        vaccinationViewModel.getVaccination()
+    }
+
 
     private fun getVaccination(screenState: ScreenState<VaccinationState>) {
         when (screenState) {
@@ -48,11 +61,21 @@ class VaccinationFragment : BaseFragment() {
         when (renderState) {
             is VaccinationState.ShowSuccess -> {
                 Log.d("josuecitoxd", "ShowSuccess: ${renderState.reg[0].s_nombre}")
-               // VaccinationState(renderState.reg)
+                vaccinationProgressBar.visibility = View.GONE
+                vaccinationList(renderState.reg)
             }
             is VaccinationState.ShowError -> {
+
                 Log.d("josuecitoxd", "error: " + renderState.reg.message)
             }
         }
+    }
+
+    private fun vaccinationList(list: List<VaccinationResponseModel>) {
+        vaccinationAdapter = VaccinationAdapter(context as Activity, list)
+        vaccinationRecyclerView.adapter = vaccinationAdapter
+        vaccinationRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+
     }
 }
