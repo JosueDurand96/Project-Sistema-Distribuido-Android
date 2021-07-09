8e5880256b36e892a9ea0ebe116ddf4498669a06
SandraChumpitaz <chumpi_84@hotmail.com>
2021-07-09 15:17:51 -0500

    data picket

--- app/src/main/java/com/durand/vacunacionperu/ui/campaign/add/AddCampaignFragment.kt
+++ app/src/main/java/com/durand/vacunacionperu/ui/campaign/add/AddCampaignFragment.kt
@@ -1,5 +1,6 @@
 package com.durand.vacunacionperu.ui.campaign.add
 
+import android.app.DatePickerDialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.durand.vacunacionperu.R
+import kotlinx.android.synthetic.main.add_campaign_fragment.*
+import java.util.*
 
 class AddCampaignFragment : Fragment() {
 
@@ -26,7 +29,26 @@ class AddCampaignFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AddCampaignViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dateImageView.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val dpd = DatePickerDialog(requireContext(), { _, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                dateTextInputEditText.setText("$dayOfMonth $monthOfYear, $year")
+            }, year, month, day)
+            dpd.show()
+        }
+
+
     }
 
 }
