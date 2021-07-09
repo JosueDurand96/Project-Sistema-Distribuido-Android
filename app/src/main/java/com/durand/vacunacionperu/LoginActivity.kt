6cf1268fa96ed224ed3aee4524ab916a704487a6
SandraChumpitaz <chumpi_84@hotmail.com>
2021-07-08 23:38:01 -0500

    logic login session

--- app/src/main/java/com/durand/vacunacionperu/LoginActivity.kt
+++ app/src/main/java/com/durand/vacunacionperu/LoginActivity.kt
@@ -6,13 +6,34 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    var usuario = ""
+    var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        usuario = userEditText.text.toString()
+        password = passwordEditText.text.toString()
+
+
         loginButton.setOnClickListener {
-            val intent = Intent(applicationContext, MenuActivity::class.java)
-            startActivity(intent)
+            initSession()
+        }
+
+    }
+
+
+    private fun initSession() {
+        if (usuario.isEmpty() && password.isEmpty()) {
+            userEditText.error = "Debe ingresar su usuario!"
+            passwordEditText.error = "Debe ingresar su contraseña!"
+        } else {
+            if (usuario == usuario) {
+                val intent = Intent(applicationContext, MenuActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
