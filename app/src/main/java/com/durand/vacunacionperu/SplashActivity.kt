fa6f6d57231deca579b90662af0b32d847a5b8f1
JosueDurand96 <josue.durand8@gmail.com>
2021-07-17 16:15:20 -0500

    animation splasj

--- app/src/main/java/com/durand/vacunacionperu/SplashActivity.kt
+++ app/src/main/java/com/durand/vacunacionperu/SplashActivity.kt
@@ -1,20 +1,45 @@
 package com.durand.vacunacionperu
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationSet
+import android.view.animation.DecelerateInterpolator
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        //TIMER
-        val duration_splash = 3000
+
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.interpolator = DecelerateInterpolator() //add this
+        fadeIn.duration = 2000
+
+        val fadeOut = AlphaAnimation(1f, 0f)
+        fadeOut.interpolator = AccelerateInterpolator() //and this
+        fadeOut.startOffset = 1000
+        fadeOut.duration = 2000
+
+        val animation = AnimationSet(true) //change to false
+        animation.addAnimation(fadeIn)
+        animation.addAnimation(fadeOut)
+        logoImageView.animation = animation
+
+        val durationSplash = 3000
         Handler().postDelayed({
-            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, duration_splash.toLong())
+            setupWindowAnimations()
+
+        }, durationSplash.toLong())
+
+    }
+    private fun setupWindowAnimations() {
+        val i = Intent(this, LoginActivity::class.java)
+        val transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(this)
+        startActivity(i, transitionActivityOptions.toBundle())
     }
 }
